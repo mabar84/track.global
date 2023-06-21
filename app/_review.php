c9e7b302aec91584dab253107d84e52416ea7392
HUAWEI <mabar@tut.by>
2023-06-21 15:19:17 +0300

    all images added

--- app/_review.php
+++
@@ -1,9 +0,0 @@
- <div class="poshta-reviews__box">
-                <span class="poshta-reviews__box-name">
-                  <?=$model->name ?>
-                </span>
-        <span class="poshta-reviews__box-star" data-rateyo-rating="<?=$model->rating ?>"></span>
-    </div>
-    <div class="poshta-reviews__item-text">
-        <?=$model->full_description ?>
-    </div>
--- app/track-by-service.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo \frontend\widgets\TrackingWidget::widget(
-    [
-        'track' => $track,
-        'client_id' => $client_id,
-        'alias' => $alias
-    ]);
